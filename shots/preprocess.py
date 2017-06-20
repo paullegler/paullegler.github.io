22f60bb9206d206fd48da9c68287a4e0e993c1f2
Paul Legler <Paul@Computer-2.local>
2017-06-19 18:53:22 -0700

    one file all the way through

--- shots/preprocess.py
+++ shots/preprocess.py
@@ -1,10 +1,22 @@
 import sys
 import openpyxl
+import os
 
-file_name = sys.argv[1];
-wb = openpyxl.load_workbook(file_name);
-print wb.get_sheet_names();
-sheet = wb.get_sheet_by_name('Sheet1');
-print sheet['D4'].value;
-print sheet['D4'];
-print sheet['D4'];
+def transfer(sheet, f):
+    row = 2
+    while (str(sheet["A" + str(row)].value) != "None"):
+        x = str(sheet["A" + str(row)].value);
+        y = str(sheet["B" + str(row)].value);
+        result = str(sheet["C" + str(row)].value);
+        f.write(x + " " + y + " " + result + "\n");
+        row = row + 1
+
+directory = "players/excel/"
+
+for filename in os.listdir(directory):
+    wb = openpyxl.load_workbook(directory + filename);
+    sheet = wb.get_sheet_by_name('Sheet1');
+    name = str("players/text/" + sheet['A1'].value) + "_" + str(sheet['B1'].value) + ".txt";
+    f= open(name,"w+");
+    transfer(sheet, f);
+    f.close();
